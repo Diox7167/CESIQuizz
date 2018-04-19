bc53559b9f565f8d26194c87d6e71521e1ff5c77
moucby <moucby@live.fr>
2018-04-19 11:45:22 +0200

    test 1

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,14 +19,5 @@ class DefaultController extends Controller
         ]);
     }
 	
-	/**
-     * @Route("/test", name="test")
-     */
-    public function test(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
-    }
+	
 }
